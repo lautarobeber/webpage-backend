91f50abde79358c0fc3b10f42cb0dc3e8e2e304c
Lautaro Beber <lautarobeber5a@gmail.com>
2024-05-08 11:14:18 -0300

    pre-dep2

--- src/db.js
+++ src/db.js
@@ -5,6 +5,8 @@ import Sequelize from "sequelize";
 const userDB = process.env.USER_DB
 const nameDB = process.env.NAME_DB
 const passwordDB = process.env.PASSWORD_DB
+const hostDB = process.env.HOST_DB
+const portDB = process.env.PORT_DB
 
 /* export const sequelize = new Sequelize(nameDB, userDB , passwordDB, {
   host: process.env.HOST_DB,
@@ -12,4 +14,4 @@ const passwordDB = process.env.PASSWORD_DB
 });
  */
 
-export const sequelize = new Sequelize('mysql://root:ZYaQxhYfhcfRiTwBAtwosEhJCvZNjLZt@roundhouse.proxy.rlwy.net:30294/railway');
+export const sequelize = new Sequelize(`mysql://${userDB}:${passwordDB}@${hostDB}:${portDB}/railway`);
